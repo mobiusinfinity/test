4245148f2b45ccaf0dc3fdbc59de07656bf47873
root <hopibee@gmail.com>
2017-08-24 03:26:17 +1000

    added more stuff

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+echo("hello world");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>some titles</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<a href="&#47;&#47;youtube.com">youtube</a>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+<br>
+<p><b>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</b>
+</p>
+</body>
+</html>
---
+++ updateScript.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+yes Y | sudo apt-get update
+
+yes Y | sudo apt-get dist-upgrade
+
+yes Y | sudo apt-get upgrade
